4c1e60d5a02d2589a978455b5fcf08ca04fb562a
Luke-Collins <95588649+Luke-Collins@users.noreply.github.com>
2022-02-10 15:18:19 +0000

    Chapt10 Refactor tests with .create

--- spec/all_spec.rb
+++ spec/all_spec.rb
@@ -1,10 +1,13 @@
 describe 'all' do
   it 'reads from the database' do
-    conn = PG.connect(dbname: 'bookmark_manager_test')
+    Bookmarks.create("http://www.makersacademy.com/")
+    Bookmarks.create("http://www.google.com/")
+    Bookmarks.create("http://www.destroyallsoftware.com/")
+    # conn = PG.connect(dbname: 'bookmark_manager_test')
 
-    conn.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com/');")
-    conn.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com/');")
-    conn.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com/');")
+    # conn.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com/');")
+    # conn.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com/');")
+    # conn.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com/');")
 
     bookmarks = Bookmarks.all
 
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -2,11 +2,15 @@ require 'pg'
 
 feature 'Bookmarks' do
   scenario 'can view bookmarks' do
-    conn = PG.connect(dbname: 'bookmark_manager_test')
+    Bookmarks.create(url: "http://www.makersacademy.com/")
+    Bookmarks.create(url: "http://www.google.com/")
+    Bookmarks.create(url: "http://www.destroyallsoftware.com/")
 
-    conn.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com/');")
-    conn.exec("INSERT INTO bookmarks VALUES(2, 'http://www.google.com/');")
-    conn.exec("INSERT INTO bookmarks VALUES(3, 'http://www.destroyallsoftware.com/');")
+    # conn = PG.connect(dbname: 'bookmark_manager_test')
+
+    # conn.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com/');")
+    # conn.exec("INSERT INTO bookmarks VALUES(2, 'http://www.google.com/');")
+    # conn.exec("INSERT INTO bookmarks VALUES(3, 'http://www.destroyallsoftware.com/');")
 
     visit '/bookmarks'
 
